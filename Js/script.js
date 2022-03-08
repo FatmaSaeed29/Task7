cbbfec04a05067d3aaedfa4f3e56d31d462586db
FatmaSaeed29 <fatmasaeedwasal@gmail.com>
2022-03-08 22:34:47 +0200

    IEEE Task7

---
+++ Js/script.js
@@ -0,0 +1,8 @@
+document.querySelector("click1").addEventListener("click" , () =>{
+    document.querySelector(".hide").classList.toggle("not")
+})
+
+
+document.querySelector(".fa-bars").addEventListener("click", () => {
+    document.querySelector(".ul").classList.toggle("here")
+})
